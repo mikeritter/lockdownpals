007fa52326ae5fbdfe0ded3955c604692b32ad6c
mike ritter <mike@mikeritteronline.com>
2020-10-03 19:13:47 -0400

    clean up typography

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,6 +1,6 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Welcome to <strong>{{ $page->title }}</strong></h1>
+    <h1>Welcome to <strong>{{ $page->title }}</strong></h1>
     <p>{{ $page->description }}</p>
 @endsection
